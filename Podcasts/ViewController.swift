639c4390fe1f63ab243364eafd18de5aa030b4e6
Danil Vereschagin <d.vereschagin@wellyes.ru>
2019-05-18 10:03:05 +0300

    Initial Commit

---
+++ Podcasts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podcasts
+//
+//  Created by We//Yes on 18/05/2019.
+//  Copyright © 2019 Daniil Vereschagin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
